f8563fbd8693db8d35dfcbc47ec11a850d9d3c47
Felipe Carvalho <P5180242@brasilprev.com.br>
2020-07-05 12:46:34 -0300

    adding new sections and metatags

--- static/script.js
+++ static/script.js
@@ -3,17 +3,25 @@
     var textSections = document.getElementsByTagName("section");
 
     for (var i = 0; i < langBtn.length; i++) {
-        langBtn[i].addEventListener("click", changeLang);
+        langBtn[i].addEventListener("click", toggleLang);
     }
 
-    function changeLang(event) {
-        var elem = event.target;
+    function toggleLang(event) {
         deactivateAllLangButtons();
+        var elem = event.target;
         elem.className += " active";
 
-        var lang = elem.getAttribute("data-lang")
         hideAllTextSections();
-        document.getElementById(lang + "-text").className = "";
+        var lang = elem.getAttribute("data-lang");
+        showSelectedLangSection(lang);
+    }
+
+    function showSelectedLangSection(lang) {
+        for (var i = 0; i < textSections.length; i++) {
+            if (textSections[i].getAttribute("data-lang") == lang + "-text") {
+                textSections[i].className = "";
+            }
+        }
     }
 
     function deactivateAllLangButtons() {
